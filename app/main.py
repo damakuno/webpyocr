a637ac1381be5ae504b6f97169631d3dfb594f38
damakuno <damakuno@gmail.com>
2020-11-06 13:29:06 +0800

    added files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='.', root_path='/home/runner')
+app = Flask(__name__, static_folder='.')
 
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+# Serves javascript files
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('./public/js', path)
+
+# Serves css style sheets
+
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('./public/css', path)
