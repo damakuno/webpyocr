7c0136f9c6eedcca4a75246a44c964d47439a661
unknown <damakuno@gmail.com>
2020-11-06 22:52:22 +0800

    added ocr

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
-from flask import Flask, send_from_directory, flash, request, redirect, url_for
+from flask import Flask, send_from_directory, flash, request, redirect, jsonify
 from werkzeug.utils import secure_filename
 import os
+# import base64
+import magic
+import pytesseract as pt
+import cv2
 
-app = Flask(__name__, static_folder='.')
+mime = magic.Magic(mime=True)
+
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 
 # @app.route('/')
@@ -34,8 +40,28 @@ def upload_file():
             return redirect('/')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join('./app/public/uploads/', filename))
-            return redirect(f'uploads/{filename}')
+            filepath = os.path.join('./app/public/uploads/images', filename)
+            file.save(filepath)
+            # return redirect(f'uploads/{filename}')
+            img_cv = cv2.imread(filepath)
+            img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+            tessdata_dir_config = r'--tessdata-dir "./app/tessdata"'
+            prediction = pt.image_to_string(img_rgb,
+                                            lang='eng',
+                                            config=tessdata_dir_config)
+
+            response = {
+                "mime_type": mime.from_file(filepath),
+                "url": f"/public/uploads/images/{filename}",
+                "prediction": prediction
+            }
+            # with open(filepath, "rb") as image:
+            #     encodedbase64 = base64.b64encode(image.read()).decode('utf-8')
+            #     response["base64raw"] = encodedbase64
+            #     response["base64uri"] = f"data:{response['mime_type']}; \
+            #         {encodedbase64}"
+
+            return jsonify(response)
 
 
 @app.route('/js/<path:path>')
@@ -48,6 +74,7 @@ def send_css(path):
     return send_from_directory('./public/css', path)
 
 
-@app.route('/uploads/<path:path>')
-def send_uploads(path):
-    return send_from_directory('./app/public/uploads', path)
+# @app.route('/uploads/<path:path>')
+# def send_uploads(path):
+#     print(os.getcwd())
+#     return send_from_directory('./app/public/uploads', path)
--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,29 +1,20 @@
-window.onload = function() {
+window.onload = function () {
     let btn_upload = document.querySelector('#btn_upload');
     let file_upload = document.querySelector('#file_upload');
     let img_upload = document.querySelector('#img_upload');
-    btn_upload.addEventListener('click', function(e) {
+    let div_prediction = document.querySelector('#div_prediction');
+    btn_upload.addEventListener('click', function (e) {
         var formData = new FormData();
         let file_name = file_upload.files[0].name;
         formData.append("image", file_upload.files[0]);
         axios.post('/upload', formData, {
             headers: {
-                'Content-Type': 'multipart/form-data',
-                'responseType': 'blob'
+                'Content-Type': 'multipart/form-data'
             }
-        }).then(function(res) {
+        }).then(function (res) {
             console.log(res);
-            blobToDataUrl(res.data).then(function(uri) {
-                img_upload.src = uri;
-            })
+            img_upload.src = res.data.url;
+            div_prediction.innerHTML = res.data.prediction;
         });
     });
-}
-
-function blobToDataUrl(blob) {
-    return new Promise(resolve => {
-        const reader = new FileReader(); // https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
-        reader.onload = e => resolve(e.target.result);
-        reader.readAsDataURL(blob);
-    })
 }
